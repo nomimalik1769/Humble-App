d92f0ffbfb5737a45231bb4c4739cc5688df91a7
NomiMalik <hammer.class82@gmail.com>
2017-11-01 17:07:55 +0500

    after adding registration forms (business,customer,technician)

--- Humble App/ViewController.swift
+++ Humble App/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cancelbtn: UIButton!
+    @IBOutlet weak var signupbtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        signupbtn.layer.cornerRadius = 20
+        cancelbtn.layer.cornerRadius = 20
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
