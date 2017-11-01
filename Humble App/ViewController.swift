870584c49d6bd2015a3faf38a065e0f8e026154c
Admin <admin@Admins-Mac.local>
2017-11-01 10:18:19 +0500

    Initial Commit

---
+++ Humble App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Humble App
+//
+//  Created by Admin on 01/11/2017.
+//  Copyright © 2017 globia Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
