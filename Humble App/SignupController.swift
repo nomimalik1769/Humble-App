10b046ce5fc62aa9209281b922aa4cb2434d78ea
NomiMalik <hammer.class82@gmail.com>
2017-11-02 11:26:24 +0500

    nm

---
+++ Humble App/SignupController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Humble App
+//
+//  Created by Admin on 01/11/2017.
+//  Copyright © 2017 globia Technologies. All rights reserved.
+//
+
+import UIKit
+
+class SigupController: UIViewController {
+
+    @IBOutlet weak var cancelbtn: UIButton!
+    @IBOutlet weak var signupbtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        signupbtn.layer.cornerRadius = 20
+        cancelbtn.layer.cornerRadius = 20
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
